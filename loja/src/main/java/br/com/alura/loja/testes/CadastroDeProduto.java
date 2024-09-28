8d71be534702bb6cbf1c14f31121574b133971f3
Cristiano de Sousa Ruas <cristiano.renuncia@gmail.com>
2024-09-28 09:50:58 -0300

    =inserindo cardialidade e mapeando relacionamento entre entidades

--- loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import br.com.alura.loja.DAO.CategoriaDAO;
 import br.com.alura.loja.DAO.ProdutoDAO;
 import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
@@ -14,12 +15,15 @@ import br.com.alura.loja.util.JPAUtil;
 public class CadastroDeProduto {
 
 	public static void main(String[] args) {
-		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), Categoria.CELULARES);
+		Categoria celulares = new  Categoria("celulares");
+		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
 		
 		EntityManager em = JPAUtil.getEntityManager();
 		ProdutoDAO produtoDAO = new ProdutoDAO(em);
+		CategoriaDAO categoriaDAO = new CategoriaDAO(em);
 		
 		em.getTransaction().begin();
+		categoriaDAO.cadastrar(celulares);
 		produtoDAO.cadastrar(celular);
 		em.getTransaction().commit();
 		em.close();
